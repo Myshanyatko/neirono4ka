a0c709bea0b9c440eff3d2509bcde770a4c3bb46
Myshanyatko <63520766+Myshanyatko@users.noreply.github.com>
2020-06-23 19:19:05 +0700

    Update neirono4ka.py

--- neirono4ka.py
+++ neirono4ka.py
@@ -1,8 +1,8 @@
 from fastai.vision import *
 from fastai.callbacks import *
 
-bs = 16
-path = Path('heroes2')
+bs = 20
+path = Path('2 heroes')
 path.ls()
 
 #np.random.seed(42)
@@ -11,25 +11,5 @@ data.normalize(imagenet_stats)
 
 data.show_batch(rows=3, figsize=(7,8))
 data.c, len(data.train_ds), len(data.valid_ds)
-
 learn = create_cnn(data, models.resnet34, metrics=error_rate)
-learn.fit_one_cycle(10, callbacks=[EarlyStoppingCallback(learn, patience=2)])
-learn.recorder.plot_losses()
-learn.recorder.plot()
-learn.lr_find()
-learn.recorder.plot()
-learn.fit_one_cycle(5, max_lr=slice(1e-3,1e-2))
-learn.recorder.plot_losses()
-learn.unfreeze()
-learn.fit_one_cycle(5, max_lr=slice(1e-8,1e-6))
-learn.recorder.plot_losses()
-
-
-#learn.save('stage-1')
-#learn.export()
-
-#img = open_image(path/'0.jpg')
-#img
-#pred_class,pred_idx,outputs = learn.predict(img)
-#print(pred_class)
-
+learn.fit_one_cycle(10)
