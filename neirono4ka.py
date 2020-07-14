c4aaac7a4f0a6c83bb5211eca86819d709b5d684
Myshanyatko <63520766+Myshanyatko@users.noreply.github.com>
2020-07-14 20:09:37 +0700

    Update neirono4ka.py

--- neirono4ka.py
+++ neirono4ka.py
@@ -1,15 +1,30 @@
 from fastai.vision import *
-from fastai.callbacks import *
+import telebot
 
-bs = 20
-path = Path('2 heroes')
-path.ls()
+bot = telebot.TeleBot('1212187995:AAGpx72h7PFM_J1ByutPZF5bUGKvpIfNWMk')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Салам пополам, это бот, который определит, кто ты из Доты. Нейросеть не ошибается, и не смей с ней спорить. Скидывай фоточку и ожидай в общем. (Фотку передадим в госдеп США потом и кредит оформим да)')
 
-#np.random.seed(42)
-data = ImageDataBunch.from_folder(path, train=path,valid_pct=0.2, ds_tfms=get_transforms(), size=224, num_workers=4, bs=bs)
-data.normalize(imagenet_stats)
+@bot.message_handler(content_types=['photo'])
+def handle_docs_photo(message):
 
-data.show_batch(rows=3, figsize=(7,8))
-data.c, len(data.train_ds), len(data.valid_ds)
-learn = create_cnn(data, models.resnet34, metrics=error_rate)
-learn.fit_one_cycle(10)
+    file_info = bot.get_file(message.photo[len(message.photo)-1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open("image.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+
+    path = r'C:\Users\пк\new neirono4ka\2 heroes'
+    learn = load_learner(path)
+    img = open_image(r'C:\Users\пк\PycharmProjects\neirono4ka\image.jpg')
+    pred_class, pred_idx, outputs = learn.predict(img)
+    card = r"""{recipient}.jpg"""
+    adr = card.format(recipient=pred_class)
+    bot.send_message(message.chat.id, 'Не верим своим глазам, вы...')
+    f = open(adr, 'rb')
+    bot.send_photo(message.chat.id, f)
+
+@bot.message_handler(content_types=['text', 'sticker', 'audio'])
+def handle_docs_text(message):
+    bot.send_message(message.chat.id, 'ебать это по твоему фотка?')
+bot.polling(none_stop=True)
